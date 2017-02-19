967d60d5871c6754d983d9e8700b328bf98bab71
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-19 21:47:56 +0100

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+   var id = req.params.id;
+
+   if (!ObjectID.isValid(id)) {
+       res.status(404).send();
+   }
+   Todo.findById(id).then((todo) => {
+       if (todo) {
+           res.send({todo});
+       } else {
+           res.status(404).send();
+       }
+   }, (err) => {
+       res.status(400).send();
+   }); 
 });
 
 app.listen(3000, () => {
