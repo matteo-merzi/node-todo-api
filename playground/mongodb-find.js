c633336a95286140060cc32853e468bcc19b8492
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-19 12:30:49 +0100

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('58a97bd379cdaf64564b720a')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Matteo'}).toArray().then((docs) => {
+        console.log(`Users count: ${docs.length}`);
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+    //db.close();
+});
